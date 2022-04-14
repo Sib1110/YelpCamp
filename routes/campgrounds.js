19073d3534859b9f1ba5dc95fdc1c21675791cfd
Wonit2 <powhonor@gmail.com>
2022-04-15 08:04:06 +0900

    Uploading images and Carousel

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,16 +3,20 @@ const router = express.Router();
 const catchAsync = require("../utils/catchAsync");
 const campgrounds = require("../controllers/campgrounds");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
-const Campground = require("../models/campground");
+const multer = require("multer");
+const { storage } = require("../cloudinary");
+const upload = multer({ storage });
 
 router
   .route("/")
   .get(catchAsync(campgrounds.index))
   .post(
     isLoggedIn,
+    upload.array("image"),
     validateCampground,
     catchAsync(campgrounds.createCampground)
   );
+
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 router
   .route("/:id")
