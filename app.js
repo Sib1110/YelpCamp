4f95394785be4b40959efd3532c10b994313baa5
Wonit2 <powhonor@gmail.com>
2022-04-21 11:22:41 +0900

    add start

--- app.js
+++ app.js
@@ -102,6 +102,8 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err });
 });
 
-app.listen(3000, () => {
-  console.log("Server is on PORT 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is on PORT ${port} `);
 });
